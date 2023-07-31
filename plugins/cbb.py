1ed4663e12a3b61bf439e00b3777d1e0c06ce8b5
nero-bc <126649764+nero-bc@users.noreply.github.com>
2023-07-31 12:12:43 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,8 +10,8 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
-            disable_web_page_preview = True,
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {version}</a>\n○ Main Channel : <a href='https://t.me/Aniwatch_to'>Click Hereafter o</a>\n○Ongoing Anime : <a href='https://t.me/Anime_Ongoing_Dub'>Click Here</a>\n○ Anime Search Group : <a href='https://t.me/AnimeDownloaderChat_Bot'>Click Here</a></b>",
+            disable_web_page_preview = True, w 
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
